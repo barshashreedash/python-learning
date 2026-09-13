1653289bd882fa4ee2f494b7183f109a8fa3122d
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-13 21:30:45 +0530

    Add datatypes.py to 01_Python_Basics
    Add a program that give the idea of datatype in python and check  how the type of the data show in output.

---
+++ 01_Python_Basics/datatypes.py
@@ -0,0 +1,14 @@
+a = 1 # a is an integer
+
+b = 7.88 # b is a floating 
+
+c = 'Shreee' # c is a string 
+
+d = False # d is a boolean variable 
+
+e = None # e is a none type variable 
+print(a)
+print(b)
+print(c)
+print(type(d))
+print(e)
