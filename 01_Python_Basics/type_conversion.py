6b9f1a4ae1d56330cecc9b5dfba11ea4e50c7f2b
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-13 22:00:31 +0530

    Add type_conversion.py to 01_Python_Basics
    Add a program say that the type conversion in python  and show that  in output .

---
+++ 01_Python_Basics/type_conversion.py
@@ -0,0 +1,9 @@
+a = 4
+b = 4.98
+
+sum= a+b # when we perform an arithmatic expression  in that time python automatically convert integer to float beacuse float is superior as compare to int.
+# 4.0 + 4.98 => 8.98
+print(sum) # implicit conversion
+e = "7"
+print(type(e))
+# sum = b+e show error beacause python can only concatenate string not "float" to string.
