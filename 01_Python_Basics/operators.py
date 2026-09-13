6a2d57bac85efddcc6c862baaca8d5542b181665
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-13 21:43:26 +0530

    Add operators.py to 01_Python_Basics
    Add a program to demonstrate the operators clearly in python .

---
+++ 01_Python_Basics/operators.py
@@ -0,0 +1,43 @@
+#  Arithmatic Operator 
+
+a = 7           
+b = 9 
+c = a + b
+print(a - b )
+print(a / b )
+print(a%b) 
+print(c)
+
+#Assignment Operators 
+a = 4 - 2 # Assign 4-2 in a 
+print(a) 
+b = 6 
+b += 3 # Increment the value of b by 3 and then assign it to b
+print(b)
+
+
+#Comparison Operators
+
+h = 5 < 4  
+print(h)
+k = 5!=6
+print(k)
+
+#Logical Operator 
+
+v = True or False
+
+#TRURTH TABLE OF 'OR'
+print("True or False is ",True or False)
+print("True or True is ",True or True)
+print("False or True  is ", False or True)
+print("False or False is ",False or False)
+
+#TRURTH TABLE OF 'AND'
+print("True and False is ",True and False)
+print("True and True is ",True and True)
+print("False and True  is ", False and True)
+print("False and False is ",False and False)
+
+#TRUTH TABLE OF 'NOT'
+print(not(True))
