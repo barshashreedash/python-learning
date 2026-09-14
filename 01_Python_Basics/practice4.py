aa33909c51f88b693c98d403d0b8dcbe7aa01f4d
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-14 19:19:56 +0530

    Add practice4.py to 01_Python_Basics
    Add a program to input 2 int numbers and print if one number is greater than or equal to another number , if not print false .

---
+++ 01_Python_Basics/practice4.py
@@ -0,0 +1,5 @@
+# WAP to input 2 int numbers , a & b .
+# Print True if a is greater than or equal to b . If not print false .
+a = int(input("Enter a number :"))
+b = int(input("Enter another number :"))
+print( a>= b)
