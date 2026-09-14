499e13b9fb5b4255f068252a50bd9d8322f9e453
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-14 19:12:29 +0530

    Add practice3.py to 01_Python_Basics
    Add a program to take input 2 floating point numbers & print their average .

---
+++ 01_Python_Basics/practice3.py
@@ -0,0 +1,4 @@
+# WAP to input 2 floating point numbers & print their average . 
+a = float(input("Enter a number :"))
+b = float(input("Enter another number :"))
+print("The avg of this 2 number is :",(a +b)/2)
