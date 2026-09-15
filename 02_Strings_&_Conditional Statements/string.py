4182e4aa56d9231d954390c7951ff487e4658674
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-15 23:58:51 +0530

    Add string.py to 02_Strings_&_Conditional Statements

---
+++ 02_Strings_&_Conditional Statements/string.py
@@ -0,0 +1,9 @@
+str1 = "This a string .\n  we are creating it in python . "# for space we use /n
+str2 = "This a string .\t  we are creating it in python . "# for tab we use /t
+print(str1)
+print(str2)
+
+
+# str = "This is a string ."
+# """This a srting."""
+# 'This is also a string .'
