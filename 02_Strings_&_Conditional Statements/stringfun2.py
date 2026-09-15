22e643de4f20c1efb74503799a103c87c3b6a412
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-16 00:13:38 +0530

    Add stringfun2.py to 02_Strings_&_Conditional Statements

---
+++ 02_Strings_&_Conditional Statements/stringfun2.py
@@ -0,0 +1,7 @@
+str1 = "This a string .\n  we are creating it in python . "
+print(str1)
+print(len(str1))# lenght function of string.
+str2="This a string ."
+str3 =" we are creating it in python . "
+print(str2 + "" + str3)
+print(len(str2 + ""+ str3 )) # lenght function of string.
