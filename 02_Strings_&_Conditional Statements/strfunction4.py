83721b4bacc23df3cfa474258db87749867f1fb8
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-16 00:34:45 +0530

    Add strfunction4.py to 02_Stringss_&_Conditional Statements

---
+++ 02_Strings_&_Conditional Statements/strfunction4.py
@@ -0,0 +1,7 @@
+str = "Private College "
+print(str[2 : 5])  # slicing function 
+str5 = "GITAM College "
+print(str5[2:5]) 
+print(str[2: ]) # is same as str[2:len(str)]
+print(str5[ :5]) # is same as str[0:5]
+ 
