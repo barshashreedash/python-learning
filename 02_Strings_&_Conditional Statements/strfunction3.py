09cc33ec928cf20ac998eb406d057379c7d7c8eb
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-16 00:24:43 +0530

    Add strfunction3.py to 02_Strings_&_Conditional Statements

---
+++ 02_Strings_&_Conditional Statements/strfunction3.py
@@ -0,0 +1,7 @@
+str = "GITAM College "
+ch = str[2]
+print(ch)
+str5 = "GOVERNMENT  College "
+# str[10] = "@" 'Str' object doesnot support item assignment .
+print(str5[10]) # print the space also.
+#In indexing form we can only access the value not manipulate .
