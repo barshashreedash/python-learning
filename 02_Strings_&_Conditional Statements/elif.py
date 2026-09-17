794eda6de8e05ee282462f2434c65119519b8362
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-17 21:13:36 +0530

    Add elif.py to 02_Strings_&_Conditional Statements

---
+++ 02_Strings_&_Conditional Statements/elif.py
@@ -0,0 +1,9 @@
+light = "green"
+if(light == "red "):#if statements always check for condition.
+        print("stop")
+elif(light == "green"):#but elif only check condition when if statements is false.
+        print("go")
+elif(light == "yellow"  ):
+        print("look")
+print("End of code")
+               
