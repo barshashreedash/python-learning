fb6aa8a1a11cb594852b53ec74a1818d33e1ccff
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-18 22:39:58 +0530

    Add list.py to 03_List_&_Tuples

---
+++ 03_List_&_Tuples/list.py
@@ -0,0 +1,9 @@
+marks = [12,6,88,87,90 ]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(marks[4])
+student = ["Karan" , 49.99 ,18 , "Delhi" ]
+print(student[0])
+student[0]="arjun"
+print(student)
