192abee925f6666e95f753a93052ad4486cbefc6
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-18 22:47:02 +0530

    Add listslicing.py to 03_List_&_Tuples

---
+++ 03_List_&_Tuples/listslicing.py
@@ -0,0 +1,5 @@
+marks = [85,44,59,99,00]
+print(marks[1:3])
+print(marks[ :3]) # is same a marks[0,3]
+print(marks[1: ]) # is same as marks[1:len(marks)]
+print(marks[-3 : -1])# negetive indexing stats with -1 & counting happen right to left
