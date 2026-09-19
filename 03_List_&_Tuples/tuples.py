7a0a9bf46d2892c8f0e5f409877d927ff4a520f6
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-19 22:27:53 +0530

    Add tuples.py to 03_List_&_Tuples

---
+++ 03_List_&_Tuples/tuples.py
@@ -0,0 +1,18 @@
+tup = (2,3,4,6)
+print(type(tup))
+print(tup[0])
+print(tup[1])
+#tup[0] = 5 # not allowed in python
+tup1 = ()# empty tuple
+print(tup1)
+print(type(tup1))
+
+tup2 = (1,)#valid tuple
+print(tup2)
+
+tup1 = (1)
+print(tup1)
+print(type(tup1))
+tup1 = ("shreee")
+print(tup1)
+print(type(tup1))
