c13ec5ab04ec05ae2420fc0da44dc399bf3f4527
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-20 09:32:36 +0530

    Add pratice1.py to 03_List_&_Tuples

---
+++ 03_List_&_Tuples/practice1.py
@@ -0,0 +1,13 @@
+# WAP to ask the user to enters name of their 3 favourite movies & store them in list .
+# a = input("Enter 3 favourite movies : ")
+# list=[a]
+# print(list) /
+movies = []
+mov1 = input("Enter 1st movies: ")
+mov2 = input("Enter 2nd movies: ")
+mov3 = input("Enter 3rd movies: ")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print (movies)
+# movies.append(input("Enter your 3 favourite Movie: ")) --> direct way 
