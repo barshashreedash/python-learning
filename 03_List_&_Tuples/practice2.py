c2d5743a7e5e06fcd0cfca10021e1d70da569b9d
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-20 09:50:06 +0530

    Add practice2.py to 03_List_&_Tuples

---
+++ 03_List_&_Tuples/practice2.py
@@ -0,0 +1,8 @@
+#WAP to check if a list contains a palindrome elements or not . 
+list= ["M","A","A","M"]
+copylist=list.copy()
+copylist.reverse()
+if(copylist == list):
+    print("It's a palindrome ")
+else:
+    print("Not a palindrome ")    
