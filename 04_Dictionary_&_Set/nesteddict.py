fa787d03c9d18875105482e03e270d2e7aacc3f9
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-21 22:52:50 +0530

    Add nesteddict.py to 04_Dictionary_&_SetD

---
+++ 04_Dictionary_&_Set/nesteddict.py
@@ -0,0 +1,12 @@
+student = {
+    "name" : "Barsha",
+    "subject" : {
+        "phy":97,
+        "chem" : 98,
+        "math" : 69,
+    }
+
+}
+print(student)
+print(student["subject"])
+print(student["subject"] ["math"])
