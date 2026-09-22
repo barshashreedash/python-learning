50df7bcc81e083c19525754a0fb654a1d353d868
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-22 23:01:25 +0530

    Add practice1.py to 04_Dictionary_&_Set

---
+++ 04_Dictionary_&_Set/practice1.py
@@ -0,0 +1,10 @@
+#Store following wor meanings in a pythondictionary:
+# tables:a piece of furniture,lists of facts & figures
+# cat:a small animal 
+dict={
+    "word" : "meaning" ,
+    "Table" : ["a piece of furniture ", "lists of facts & figures"],
+    "cat" : "a small animal"
+    
+}
+print(dict)
