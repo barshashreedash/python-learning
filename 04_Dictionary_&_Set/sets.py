7d806cb4bd9a0eb064924792c137422242f25dff
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-22 22:00:48 +0530

    Add sets.py to 04_Dictionary_&_Set

---
+++ 04_Dictionary_&_Set/sets.py
@@ -0,0 +1,8 @@
+collection = {1,2,3,4,"hello","world","world",4} #repeated items are stored only once 
+print(collection)
+print(len(collection)) # total numbers of items
+print(type(collection))
+# collection1= {}# empty dictionary
+# print(type(collection1))
+collection1 = set() # empty set syntax
+print(type(collection1))
