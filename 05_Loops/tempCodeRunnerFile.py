41856c73ba182f5e00ace3573a4582217c1f74da
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-24 11:20:57 +0530

    Add whileloop.py to 05_Loops

---
+++ 05_Loops/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+while count <= 5:
+    print("Hello")
+    count += 1
+
+print(count) 
+i = 1 
+while i <= 100:
+    print("Python",i)
+    i += 1 
+
+print(i)       
---
+++ 05_Loops/whileloop.py
@@ -0,0 +1,23 @@
+count = 1
+while count <= 5:
+    print("Hello")
+    count += 1
+
+print(count) 
+i = 1 
+while i <= 100:
+    print("Python",i)
+    i += 1 
+
+print(i)       
+    # print numbers 1 to 5
+i = 1
+while i <= 5:
+    print(i)  
+    i += 1
+print("Loop Ended")    
+# print numbers 5 to 1    
+j = 5
+while j >= 1 :
+    print(j)
+    j -= 1 
