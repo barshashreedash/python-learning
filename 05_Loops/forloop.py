59571d7f2ef1cbb742400a738358192029b22820
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-26 21:25:11 +0530

    Add forloop.py to 05_Loops

---
+++ 05_Loops/forloop.py
@@ -0,0 +1,11 @@
+num = [1,2,4,5,6]
+for val in num:
+    print(val)
+veggies =  ["potato","brinjal","ladyfinger","cucumber"]
+for val in veggies:
+    print(val)
+str = "GITAM COLLEGE"
+for char in str: # for loop with else
+    print(char)
+else:
+    print("end") # work when loop ends 
