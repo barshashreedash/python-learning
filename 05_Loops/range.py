2b21c220c62e4c5271bf524bea9594cb8273ebce
barshashreedash-prog <barshashreedash@gmail.com>
2026-09-26 21:43:41 +0530

    Add range.py to 05_Loops

---
+++ 05_Loops/range.py
@@ -0,0 +1,14 @@
+seq = range(7)
+# print(seq[0]) this s the normal way 
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+# print(seq[4])
+# print(seq[5])
+# print(seq[6])
+for i in seq: # without using range 
+    print(i)
+for i in range(2,10):# range(start,stop)
+    print(i)
+for i in range(2,10,2): # range(start, stop,stepsize )
+    print(i)
